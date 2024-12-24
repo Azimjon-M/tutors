8d9d7d65f4eb11e4b528872cbd30c82efe6bae51
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-12-24 13:24:13 +0500

    baholash mezonlari++

--- src/components/BaholashCom/index.jsx
+++ src/components/BaholashCom/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const BaholashCom = () => {
   return (
-    <div>
-      <h2>Baholash</h2>
+    <div className="max-w-[1600px] mx-auto">
+      <h1 className="text-3xl font-medium text-gray-700 dark:text-white text-center my-5">
+        Baholash
+      </h1>
+      <div className="max-w-7xl px-5 mx-auto">nilubhil</div>
     </div>
   );
 };
