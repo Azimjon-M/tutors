7c8146857b2913ca53f701b2553db77bb14dd7de
Orifxon3595 <orifxontuychiyev@gmail.com>
2025-01-28 16:34:04 +0500

    update

--- src/components/TTJgaTashrifCom/index.jsx
+++ src/components/TTJgaTashrifCom/index.jsx
@@ -4,36 +4,14 @@ import rasm1 from "../../assets/fon/imagess.jpg";
 import rasm2 from "../../assets/fon/university-events-two.jpg";
 
 function TTJgaTashrifCom() {
-  // const datas = [
-  //   {
-  //     id: 1,
-  //     title: "TTJga tashrif",
-  //     date: "12.02.2025",
-  //     grade: 9,
-  //     image_one: "",
-  //     image_two: "",
-  //     description:
-  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "TTJga tashrif",
-  //     date: "12.02.2025",
-  //     grade: 9,
-  //     image_one: "",
-  //     image_two: "",
-  //     description:
-  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  //   },
-  // ];
 
   return (
-    <div className="border border-gray-400 rounded-md shadow-md shadow-gray-200 mt-5 p-3">
+    <div className="border border-gray-300 rounded-md shadow-md shadow-gray-200 mt-5 p-3">
       <div className="collapse collapse-arrow rounded-md bg-white mb-3">
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -94,7 +72,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -155,7 +133,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -216,7 +194,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -277,7 +255,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -338,7 +316,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -399,7 +377,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -460,7 +438,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
@@ -521,190 +499,7 @@ function TTJgaTashrifCom() {
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title md:text-xl font-medium">
           <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
-              Click to open this one and close others
-            </h4>
-            <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
-              7
-            </p>
-          </div>
-        </div>
-        <div className="collapse-content">
-          <div className="bg-slate-100 rounded-md p-3">
-            <div className="flex items-center justify-between">
-              <p className="text-sm md:text-base text-slate-600">
-                22.02.2025 yil
-              </p>
-              <button className="flex items-center gap-1 px-4 py-1 rounded-md bg-green-500 text-white text-sm md:text-base font-semibold">
-                Yuklash olish{" "}
-                <MdOutlineCloudDownload className="inline text-xl md:text-2xl" />
-              </button>
-            </div>
-            <div className="px-5 pt-5">
-              <h1 className="text-xl md:text-2xl font-semibold text-slate-600 text-center">
-                Click to open this one and close others
-              </h1>
-            </div>
-            <div className="grid md:grid-cols-2 items-center gap-5 mt-5 px-5">
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm1}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm2}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-            </div>
-            <div className="p-5">
-              <p className="md:text-xl text-slate-600">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow rounded-md bg-white mb-3">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title md:text-xl font-medium">
-          <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
-              Click to open this one and close others
-            </h4>
-            <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
-              7
-            </p>
-          </div>
-        </div>
-        <div className="collapse-content">
-          <div className="bg-slate-100 rounded-md p-3">
-            <div className="flex items-center justify-between">
-              <p className="text-sm md:text-base text-slate-600">
-                22.02.2025 yil
-              </p>
-              <button className="flex items-center gap-1 px-4 py-1 rounded-md bg-green-500 text-white text-sm md:text-base font-semibold">
-                Yuklash olish{" "}
-                <MdOutlineCloudDownload className="inline text-xl md:text-2xl" />
-              </button>
-            </div>
-            <div className="px-5 pt-5">
-              <h1 className="text-xl md:text-2xl font-semibold text-slate-600 text-center">
-                Click to open this one and close others
-              </h1>
-            </div>
-            <div className="grid md:grid-cols-2 items-center gap-5 mt-5 px-5">
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm1}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm2}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-            </div>
-            <div className="p-5">
-              <p className="md:text-xl text-slate-600">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow rounded-md bg-white mb-3">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title md:text-xl font-medium">
-          <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
-              Click to open this one and close others
-            </h4>
-            <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
-              7
-            </p>
-          </div>
-        </div>
-        <div className="collapse-content">
-          <div className="bg-slate-100 rounded-md p-3">
-            <div className="flex items-center justify-between">
-              <p className="text-sm md:text-base text-slate-600">
-                22.02.2025 yil
-              </p>
-              <button className="flex items-center gap-1 px-4 py-1 rounded-md bg-green-500 text-white text-sm md:text-base font-semibold">
-                Yuklash olish{" "}
-                <MdOutlineCloudDownload className="inline text-xl md:text-2xl" />
-              </button>
-            </div>
-            <div className="px-5 pt-5">
-              <h1 className="text-xl md:text-2xl font-semibold text-slate-600 text-center">
-                Click to open this one and close others
-              </h1>
-            </div>
-            <div className="grid md:grid-cols-2 items-center gap-5 mt-5 px-5">
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm1}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-              <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
-                <img
-                  src={rasm2}
-                  alt="university events"
-                  className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
-                />
-              </div>
-            </div>
-            <div className="p-5">
-              <p className="md:text-xl text-slate-600">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow rounded-md bg-white mb-3">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title md:text-xl font-medium">
-          <div className="flex items-center justify-between">
-            <h4 className="li line-clamp-1">
+            <h4 className="text-gray-600 line-clamp-1">
               Click to open this one and close others
             </h4>
             <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
--- src/pages/MajburiyTopshiriqlar/TTJgaTashrifTutor/index.jsx
+++ src/pages/MajburiyTopshiriqlar/TTJgaTashrifTutor/index.jsx
@@ -1,61 +1,72 @@
 import React from "react";
 import TTJgaTashrifCom from "../../../components/TTJgaTashrifCom";
+import laptopGirl from "../../../assets/fon/laptop-girl.png";
 
 function TTJgaTashrifTutor() {
   return (
     <div>
       <div className="max-w-7xl mx-auto px-5">
-        <div className="flex justify-start bg-white rounded-md shadow-md shadow-gray-200 mt-8 p-3">
-          <div
-            class="flex relative w-2/4 h-6 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700"
-            role="progressbar"
-            aria-valuenow="25"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            <div
-              class="flex flex-col justify-center overflow-hidden bg-blue-300 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500 w-2/4"
-            ></div>
-            <div class="text-center absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
-              <span class="text-center text-base font-bold text-blue-500 dark:text-blue-500">
-                5 | 22:55:50
-              </span>
+        <div className="grid grid-cols-3 bg-white rounded-md shadow-md shadow-gray-200 mt-8 p-3">
+          <div className="pl-5 pt-5">
+            <h2 className="text-xl text-gray-600">Talabalar turar joyiga tashrif sahifasi</h2>
+            <h1 className="text-2xl text-gray-600 mt-2">Hush kelibsiz😃, <span className="text-indigo-600 font-semibold">Tyutor</span></h1>
+            <p className=" text-gray-600 mt-2">Platformaga topshiriqlarni o'z vaqtida yuklashingizni so'raymiz, vaqtingiz chegaralangan!</p>
+            <button className="w-full rounded-md bg-indigo-500 mt-5 py-1 text-white">1/2</button>
+            {/* <div
+              class="flex relative h-6 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700"
+              role="progressbar"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <div class="flex flex-col justify-center overflow-hidden bg-blue-300 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500 w-2/4"></div>
+              <div class="text-center absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                <span class="text-center text-base font-bold text-blue-500 dark:text-blue-500">
+                  5 | 22:55:50
+                </span>
+              </div>
+            </div> */}
+          </div>
+          <div className="flex items-center justify-center">
+            <img src={laptopGirl} alt="Laptop girl" />
+          </div>
+          <div className="flex items-center justify-center border-l-2">
+            <div class="relative text-center size-44">
+              <svg
+                class="size-full -rotate-90"
+                viewBox="0 0 36 36"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="18"
+                  cy="18"
+                  r="16"
+                  fill="none"
+                  class="stroke-current text-indigo-200 dark:text-neutral-700"
+                  stroke-width="1"
+                ></circle>
+
+                <circle
+                  cx="18"
+                  cy="18"
+                  r="16"
+                  fill="none"
+                  class="stroke-current text-indigo-500 dark:text-blue-500"
+                  stroke-width="2"
+                  stroke-dasharray="100"
+                  stroke-dashoffset="40"
+                  stroke-linecap="round"
+                ></circle>
+              </svg>
+
+              <div class="text-center absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                <span class="text-center text-4xl font-bold text-indigo-500 dark:text-blue-500">
+                  124
+                </span>
+                <p className="text-center text-4xl font-bold text-indigo-500 dark:text-blue-500">soat</p>
+              </div>
             </div>
           </div>
-          {/* <div class="relative text-center size-28">
-            <svg
-              class="size-full -rotate-90"
-              viewBox="0 0 36 36"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="18"
-                cy="18"
-                r="16"
-                fill="none"
-                class="stroke-current text-gray-200 dark:text-neutral-700"
-                stroke-width="2"
-              ></circle>
-
-              <circle
-                cx="18"
-                cy="18"
-                r="16"
-                fill="none"
-                class="stroke-current text-blue-600 dark:text-blue-500"
-                stroke-width="2"
-                stroke-dasharray="100"
-                stroke-dashoffset="10"
-                stroke-linecap="round"
-              ></circle>
-            </svg>
-
-            <div class="text-center absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
-              <span class="text-center text-xl font-bold text-blue-600 dark:text-blue-500">
-                22:55:50
-              </span>
-            </div>
-          </div> */}
         </div>
         <TTJgaTashrifCom />
       </div>
