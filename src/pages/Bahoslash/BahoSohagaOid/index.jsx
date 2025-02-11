5b74153ca8ac4a8e35c39e60918157a4f4be521b
Azimjon-M <azimeliboev2000@gmail.com>
2025-02-11 09:23:14 +0500

    mock.js + for test, BahoSohagaOid ++, BahoQoshimcha ++, BahoOzTashabbus ++,

--- src/pages/Bahoslash/BahoSohagaOid/index.jsx
+++ src/pages/Bahoslash/BahoSohagaOid/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import BahoTable from "../../../components/BahoTable";
+import { data } from "../../../mock";
 
 const BahoSohagaOid = () => {
-    return (
-        <div>BahoSohagaOid</div>
-    )
-}
+    return <BahoTable data={data} totalPages={10} taskDate={`2025-02-03`} />;
+};
 
-export default BahoSohagaOid
+export default BahoSohagaOid;
