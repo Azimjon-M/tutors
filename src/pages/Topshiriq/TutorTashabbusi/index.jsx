f5f2b2f4ad7169ad88173999a4e71ed858e5dd0d
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-02-14 12:46:29 +0500

    tashabbus

--- src/pages/Topshiriq/TutorTashabbusi/index.jsx
+++ src/pages/Topshiriq/TutorTashabbusi/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import APITopshiriq from "../../../services/qoshimchaTopshiriqTutor";
+import APIOzXoshishi from "../../../services/ozXohishBilanTutor";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import rasm1 from "../../../assets/fon/university-events.jpg";
+import rasm2 from "../../../assets/fon/university-events-two.jpg";
 
 const TutorTashabbusi = () => {
   const [showModal, setShowModal] = useState(false);
-  const [initialValues, setInitialValues] = useState({
+  const [initialValues] = useState({
+    user: "",
     title: "",
     body: "",
     file1: "",
@@ -38,6 +41,7 @@ const TutorTashabbusi = () => {
 
   const handleSubmit = async (values, { resetForm }) => {
     const formData = new FormData();
+    formData.append("user", values.title);
     formData.append("title", values.title);
     formData.append("body", values.body);
     if (values.file1) formData.append("file1", values.file1);
@@ -45,7 +49,7 @@ const TutorTashabbusi = () => {
     if (values.file3) formData.append("file3", values.file3);
 
     try {
-      await APITopshiriq.post(formData);
+      await APIOzXoshishi.post(formData);
       resetForm();
       setShowModal(false);
     } catch (error) {
@@ -77,16 +81,98 @@ const TutorTashabbusi = () => {
         {/* Background Decoration */}
         <div className="absolute inset-0 bg-gradient-to-r from-blue-200 to-transparent opacity-50 pointer-events-none"></div>
       </div>
-      <div className="my-3">
-        <h2 className="text-2xl lg:text-3xl font-medium text-center">
-          Tashabbuslar
-        </h2>
-        <div className="text-red-500 text-xl italic text-center my-5">
-          Tashabbuslar mavjud emas.!
+      <div className="max-w-7xl mx-auto px-5">
+        <div className="my-3">
+          <h2 className="text-2xl lg:text-3xl font-medium text-center">
+            Tashabbuslar
+          </h2>
+          <div className="text-red-500 text-xl italic text-center my-5">
+            Tashabbuslar mavjud emas.!
+          </div>
+        </div>
+        {/* 📌 Tashabbuslar */}
+        <div className="border border-gray-300 space-y-3 rounded-md shadow-md shadow-gray-200 mt-5 p-3 mx-3">
+          <div className="collapse collapse-arrow rounded-md bg-white">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title md:text-xl font-medium">
+              <div className="flex items-center justify-between">
+                <h4 className="text-gray-600 line-clamp-1">
+                  Click to open this one and close others
+                </h4>
+                <p className="w-7 h-7 rounded-md text-center leading-7 bg-emerald-200 text-emerald-600">
+                  7
+                </p>
+              </div>
+            </div>
+            <div className="collapse-content">
+              <div className="bg-slate-100 rounded-md p-3">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm md:text-base text-slate-600">
+                    22.02.2025 yil
+                  </p>
+                </div>
+                <div className="px-5 pt-5">
+                  <h1 className="text-xl md:text-2xl font-semibold text-slate-600 text-center">
+                    Click to open this one and close others
+                  </h1>
+                </div>
+                <div className="grid md:grid-cols-2 items-center gap-5 mt-5 px-5">
+                  <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
+                    <img
+                      src={rasm1}
+                      alt="university events"
+                      className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
+                    />
+                  </div>
+                  <div className="flex justify-center items-center border p-3 border-slate-300 rounded-md bg-slate-200">
+                    <img
+                      src={rasm2}
+                      alt="university events"
+                      className="h-52 md:h-72 object-cover rounded-md shadow-2xl"
+                    />
+                  </div>
+                </div>
+                <div className="p-5">
+                  <p className="md:text-xl text-slate-600">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div className="flex items-center pl-5">
+                  <a
+                    className="inline-flex items-center gap-x-1 text-sm md:text-base text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500"
+                    href="https://www.kspi.uz"
+                  >
+                    Qo'shimcha xujjat fayl
+                    <svg
+                      className="shrink-0 size-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="m9 18 6-6-6-6" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      {/* 📌 Tashabbuslar */}
-      <div>Tashabbus</div>
 
       {/* 📌 Modal */}
       <div
